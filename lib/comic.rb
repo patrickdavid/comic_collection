a216707c35561c2c632ebe619b2d3090b83c2274
Patrick David <Patrick.david3@gmail.com>
2014-08-08 05:19:13 +0000

    User can edit the condition of a comic book

--- lib/comic.rb
+++ lib/comic.rb
@@ -1,15 +1,20 @@
 class Comic
   
-  attr_reader :title, :issue_num, :worth
+  attr_reader :title, :issue_num, :worth, :cgi
   
   def initialize(property)
     @title = property['title']
-    @isssue_num = property[issue_num].to_i
-    @worth = property[worth]
+    @isssue_num = property['issue_num'].to_i
+    @worth = property['worth'].to_f
+    @cgi = property['cgi'].to_f
   end
   
   def edit_worth(new_worth)
     @worth = new_worth
   end
-end
   
+  def edit_grade(condition)
+    @cgi = condition
+  end
+  
+end
--- spec/comic_spec.rb
+++ spec/comic_spec.rb
@@ -8,8 +8,14 @@ describe 'Comic' do
   end
   
   it 'allows a user to edit a comic books worth' do
-    new_comic = Comic.new({'title'=>'The Walking Dead', 'issue_num'=> 1,  'worth'=>999.99})
+    new_comic = Comic.new({'title'=>'The Walking Dead', 'issue_num'=> '1',  '@cgi'=>"9.2", 'worth'=>'999.99'})
     new_comic.edit_worth(1000.00)
     expect(new_comic.worth).to eq 1000.00
+  end
+  
+  it 'allows a user to edit a comic books condition ie if it goes from a near mint grade to very good grade' do
+    new_comic = Comic.new({'title'=>'The Walking Dead', 'issue_num'=> '1',  '@cgi'=>"9.2", 'worth'=>'999.99'})
+    new_comic.edit_grade(8.5)
+    expect(new_comic.cgi).to eq 8.5
   end
 end
