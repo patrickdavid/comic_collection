c1f3d7b2f6110bb621851bb81d0eafcb946e89d7
Patrick David <Patrick.david3@gmail.com>
2014-08-08 04:27:35 +0000

    User can save multiple instances of the comic collection class

--- lib/comic_collection.rb
+++ lib/comic_collection.rb
@@ -1,9 +1,17 @@
 class Comic_collection
   
-  @@Comic_collection = []
+  @@all_comics = []
+  
+  def self.all
+    @@all_comics
+  end
   
   def initialize(publisher)
     @publisher = publisher
     @comics = []
+  end
+  
+  def save
+    @@all_comics << self
   end
 end
--- spec/comic_collection_spec.rb
+++ spec/comic_collection_spec.rb
@@ -5,4 +5,12 @@ describe 'Comic_collection' do
   it 'creates an instance of Comic collection by publisher' do
     Comic_collection.new('Image').should be_an_instance_of Comic_collection
   end
+  
+  it 'displays all comics' do
+    new_collection1 = Comic_collection.new('Image')
+    new_collection1.save
+    new_collection2 = Comic_collection.new('Marvel')
+    new_collection2.save
+    expect(Comic_collection.all).to eq [new_collection1, new_collection2]
+  end
 end
